49620f508b51a3df4553aa40bd0d6113883b94ce
Evan Kysley <e.kysley@gmail.com>
2018-06-13 14:42:18 -0400

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-import { HeaderWrapper } from 'Styled/Header'
+import { HeaderWrapper, HeaderLinks, HeaderLink } from 'Styled/Header'
 import wblogo from '../assets/images/wb-logo-vector.svg'
 
 const Header = () => (
   <HeaderWrapper>
-    <img src={wblogo} alt="Whiteblock" width={50} height={50} />
+    <HeaderLinks>
+    <Link to="/">
+      <img src={wblogo} alt="Whiteblock" width={50} height={50} />
+    </Link>
+      <HeaderLink to="/about">About</HeaderLink>
+    </HeaderLinks>
   </HeaderWrapper>
 )
 
--- src/styled/Header.js
+++ src/styled/Header.js
@@ -1,17 +1,37 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-const HeaderWrapper = styled.div`
+import { theme } from 'Styled/Utils'
+
+const HeaderWrapper = styled.header`
   display: inline-block;
   padding-left: 15vw;
   padding-top: 5vh;
 `
 
-const Logo = styled.img`
-  width: 50px;
-  height: 50px;
+const HeaderLinks = styled.div`
+  /* position: fixed; */
+  display: flex;
+  width: 10vw;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: ${theme.color.white};
+`
+
+const HeaderLink = styled(Link)`
+  color: ${theme.color.darkPurple};
+  text-decoration: none;
+  font-size: ${theme.font.button};
+  transition: all .3s ease-in-out;
+
+  &:hover {
+    color: ${theme.color.altSecondary};
+  }
 `
 
 export {
   HeaderWrapper,
-  Logo,
+  HeaderLinks,
+  HeaderLink,
 }
